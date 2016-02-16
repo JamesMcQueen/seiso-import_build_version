ef9c0043c59694fdfe8d721a080de33d302ad53d
jmcqueen <jmcqueen@expedia.com>
2016-02-16 14:11:01 -0800

    clean and polish

--- src/main/java/com/expedia/seisoimport/utils/BuildVersionSettings.java
+++ src/main/java/com/expedia/seisoimport/utils/BuildVersionSettings.java
@@ -1,7 +1,10 @@
-package com.expedia.seisoimport.services;
+package com.expedia.seisoimport.utils;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Level;
 
 /**
  * Author: James McQueen (jmcqueen@expedia.com)
@@ -11,35 +14,15 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "buildVersionSettings")
 public class BuildVersionSettings
 {
-	private String logSuccess;
-	private String logFailure;
 	private String findByNameURL;
 	private String fields;
 	private boolean isActive = false;
+	private String username;
+	private String password;
 
 	public BuildVersionSettings()
 	{
 		// no args constructor
-	}
-
-	public String getLogSuccess()
-	{
-		return logSuccess;
-	}
-
-	public void setLogSuccess(String logSuccess)
-	{
-		this.logSuccess = logSuccess;
-	}
-
-	public String getLogFailure()
-	{
-		return logFailure;
-	}
-
-	public void setLogFailure(String logFailure)
-	{
-		this.logFailure = logFailure;
 	}
 
 	public String getFindByNameURL()
@@ -70,5 +53,25 @@ public class BuildVersionSettings
 	public void setActive(boolean isActive)
 	{
 		this.isActive = isActive;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
 	}
 }
